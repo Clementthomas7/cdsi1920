da001fb169d65c1a696e5f33b432054ae65a5f3b
Clementthomas7 <62551185+Clementthomas7@users.noreply.github.com>
2020-03-23 15:03:49 +0100

    Update

--- test.php
+++ test.php
@@ -9,7 +9,7 @@
 <body>
     <?php
         echo "<h3>Aujourd'hui le ".date("d/M/Y h:i:s")."</h3><hr>";
-        echo "<h2>Bienvenue sur le site PHP 7</h2>";
+        echo "<h2>Bienvenue sur le site PHP 7.2</h2>";
     ?>
 </body>
 </html>
